a38dd389b0fa8a54ffda95bd3ccd90d1ad693df0
Tom Emery <tom@odissei-data.nl>
2023-04-05 22:59:11 +0200

    Add files via upload

---
+++ ExistingQuantData_1.R
@@ -0,0 +1,13 @@
+##install.packages('tidyverse')
+##install.packages('epiDisplay')
+
+library('tidyverse')
+library('haven')
+library('ggplot2')
+library('epiDisplay')
+
+setwd('')
+
+ess6 <- read_dta('')
+
+View(ess6)
